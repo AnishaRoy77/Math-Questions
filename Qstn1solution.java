e36c05dd77b2152b77f8d1808c62cccab7e7265a
jaswant singh <76163062+jaswantjasu@users.noreply.github.com>
2022-03-09 16:57:57 +0530

    Update Qstn1solution.java

--- Qstn1solution.java
+++ Qstn1solution.java
@@ -1,14 +1,1 @@
-public class Qstn1 {
-    public static void main(String[] args) {
-        double answer = solve();
-        System.out.println("Answer : " + Math.round(answer));
-    }
 
-    public static double solve() {
-        double x = 0;
-        for(int i = 1 ; i <= 89 ; i++){
-            x += Math.log10(Math.tan(Math.toRadians(i)));
-        }
-        return x;
-    }
-}
